06491ab7f9982ab0df03e7a61e70a244e4dee1b8
ytakasugi <sh7.tibi0129@gmail.com>
2021-06-26 00:48:59 +0900

    new cargo project

---
+++ rustlings/exercises/collections/hashmap002/src/main.rs
@@ -0,0 +1,28 @@
+use std::collections::HashMap;
+
+#[derive(Hash, PartialEq, Eq)]
+enum Fruit {
+    Apple,
+    Banana,
+    Mango,
+    Lychee,
+    Pineapple,
+}
+
+fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
+    let fruit_kind = vec![
+        Fruit::Apple,
+        Fruit::Banana,
+        Fruit::Mango,
+        Fruit::Lychee,
+        Fruit::Pineapple,
+    ];
+
+    for fruit in fruit_kind {
+        todo!()
+    }
+}
+
+fn main() {
+    println!("Hello, world!");
+}
