c1cc1f18b71e00ad5a6702ce520c84d98cabcc12
ytakasugi <sh7.tibi0129@gmail.com>
2021-07-11 00:45:00 +0900

    change files

--- Atcoder/Contest/beginner/209th/a209_counting/target/rls/debug/deps/a209_counting-10a2800300811e4b.d
+++
@@ -1,5 +0,0 @@
-/home/ytakasugi/004_learning_rust/Atcoder/Contest/beginner/209th/a209_counting/target/rls/debug/deps/a209_counting-10a2800300811e4b.rmeta: src/main.rs
-
-/home/ytakasugi/004_learning_rust/Atcoder/Contest/beginner/209th/a209_counting/target/rls/debug/deps/a209_counting-10a2800300811e4b.d: src/main.rs
-
-src/main.rs:
