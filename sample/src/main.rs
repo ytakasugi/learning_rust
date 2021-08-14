517a07942af7334ee34a328040a56f08ff0db0ca
ytakasugi <sh7.tibi0129@gmail.com>
2021-08-14 14:40:55 +0900

    change 2 files and remove file

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,3 +1,27 @@
+use std::thread;
+use std::sync::mpsc;
+use std::time::Duration;
+
 fn main() {
-    println!("Hello World!");
+    let (tx, rx) = mpsc::channel();
+
+    thread::spawn(move || {
+        let vals = vec![
+            String::from("Rust"),
+            String::from("is"),
+            String::from("a"),
+            String::from("well"),
+            String::from("loved"),
+            String::from("language")
+        ];
+
+        for i in vals {
+            tx.send(i).unwrap();
+            thread::sleep(Duration::from_secs(1));
+        }
+    });
+
+    for r in rx {
+        println!("Got: {}", r);
+    }
 }
