9f8927bd94d77a818726b589aecb2ee30011e92e
ytakasugi <sh7.tibi0129@gmail.com>
2021-09-24 00:15:42 +0900

    change files

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello World");
+use axum::prelude::*;
+use std::net::SocketAddr;
+
+#[tokio::main]
+async fn main() {
+    let app = route("/", get(root));
+    let addr = SocketAddr::from(([127, 0, 0, 1], 3000));
+    hyper::Server::bind(&addr)
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
+}
+
+async fn root() -> &'static str {
+    "Hello, World!"
 }
