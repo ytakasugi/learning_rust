c18776fdcdb968aaa02d688e1c361e5d0fe454f0
ytakasugi <sh7.tibi0129@gmail.com>
2021-10-13 23:29:48 +0900

    change file

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -2,17 +2,15 @@ use proconio::input;
 fn main() {
     input! {
         n: i32,
-        sum: i32,
     }
 
-    for i in 0..=n {
-        for j in 0..=n {
-            let z = n - i - j;
-            if z >= 0 && i * 10000 + j * 5000 + z * 1000 == sum {
-                println!("{} {} {}", i, j, z);
+    for i in 1..10 {
+        for j in 1..10 {
+            if i * j == n {
+                println!("Yes");
                 return;
             }
         }
     }
-    println!("{} {} {}", -1, -1, -1);
+    println!("No");
 }
