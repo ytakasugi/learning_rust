fdd2318467bfca90fb874f71df08fc8c5544e2e9
ytakasugi <sh7.tibi0129@gmail.com>
2021-10-31 00:18:35 +0900

    new carog project

---
+++ basics/practice/app/mini-crawler/src/main.rs
@@ -0,0 +1,15 @@
+use select::document::Document;
+use select::predicate::Name;
+
+fn main() -> eyre::Result<()> {
+    let body = reqwest::blocking::get("https://www.rust-lang.org")?
+        .text()?;
+    
+    let doc = Document::from(body.as_str());
+
+    for a in doc.find(Name("a")) {
+        println!("{:?}", a);
+    }
+
+    Ok(())
+}
