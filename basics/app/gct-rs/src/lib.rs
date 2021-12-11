980af4d36fb1a850cc56c87540504560e2b87790
ytakasugi <sh7.tibi0129@gmail.com>
2021-12-12 00:07:04 +0900

    change files

--- basics/app/gct-rs/src/lib.rs
+++ basics/app/gct-rs/src/lib.rs
@@ -6,19 +6,32 @@ use std::fs::File;
 extern crate csv;
 extern crate serde_derive;
 
-type Record = Vec<(String, i32)>;
+use chrono::offset::{Local, TimeZone};
+use chrono::Date;
 
-pub fn run() -> Result<(), Box<dyn Error>> {
+type Record = (String, i32);
+
+pub fn run() -> Result<Vec<(String, i32)>, Box<dyn Error>> {
     let file_path = get_first_arg()?;
     let file = File::open(file_path)?;
     let mut rdr = csv::Reader::from_reader(file);
 
+    let mut v = Vec::new();
+
     for result in rdr.deserialize() {
         let record: Record = result?;
-        println!("{:?}", record);
+        v.push(record);
     }
+    Ok(v)
+}
 
-    Ok(())
+pub fn parse_time(time_str: &str) -> Date<Local> {
+    Local.datetime_from_str(
+        &format!("{} 0:0", time_str),
+        "%Y-%m-%d %H:%M"
+    )
+    .unwrap()
+    .date()
 }
 
 fn get_first_arg() -> Result<OsString, Box<dyn Error>> {
--- basics/app/gct-rs/src/main.rs
+++ basics/app/gct-rs/src/main.rs
@@ -1,10 +1,78 @@
-use std::process;
+//use gct_rs::{run, parse_time};
 
-use gct_rs::run;
+use plotters::prelude::*;
+use chrono::offset::Local;
+use chrono::Date;
 
-fn main() {
-    if let Err(err) = run() {
-        println!("{}", err);
-        process::exit(1);
-    }
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+
+/* (1) プロット用データの準備 */
+
+// データを取得。この時点では(日付,値)のタプルのVector型になっている
+let data = gct_rs::run()?;
+
+/* x軸とy軸で個別のVector型にする */
+
+// x軸 : 日付のVector
+let xs: Vec<Date<Local>> = data.iter()
+                            .map(|(x, _)| gct_rs::parse_time(&*x))
+                            .collect();
+// y軸: 値のVector
+let ys: Vec<i32> = data.iter()
+                    .map(|(_, y)| *y)
+                    .collect();
+
+
+/* (2) 描画先の情報を設定 */
+
+let image_width = 1080;
+let image_height = 720;
+// 描画先を指定。画像出力する場合はBitMapBackend
+let root = BitMapBackend::new
+    ("plot.png", (image_width, image_height)).into_drawing_area();
+
+// 背景を白にする
+root.fill(&WHITE)?;
+
+
+/* (3) グラフ全般の設定 */
+
+/* y軸の最大最小値を算出
+f32型はNaNが定義されていてys.iter().max()等が使えないので工夫が必要
+ */
+let (y_min, y_max) = ys.iter()
+                        .fold(
+                        (0, 0),
+                        |(m,n), v| (*v.min(&m), *v.max(&n))
+                        );
+
+let caption = "Sample Plot";
+let font = ("sans-serif", 20);
+
+let mut chart = ChartBuilder::on(&root)
+    .caption(caption, font.into_font()) // キャプションのフォントやサイズ
+    .margin(10)                         // 上下左右全ての余白
+    .x_label_area_size(16)              // x軸ラベル部分の余白
+    .y_label_area_size(42)              // y軸ラベル部分の余白
+    .build_cartesian_2d(                // x軸とy軸の数値の範囲を指定する
+      *xs.first().unwrap()..*xs.last().unwrap(), // x軸の範囲
+      y_min..y_max                               // y軸の範囲
+    )?;
+
+
+  /* (4) グラフの描画 */
+
+  // x軸y軸、グリッド線などを描画
+chart.configure_mesh().draw()?;
+
+  // 折れ線グラフの定義＆描画
+let line_series = LineSeries::new(
+                    xs.iter()
+                    .zip(ys.iter())
+                      .map(|(x, y)| (*x, *y)),
+                    &RED
+);
+chart.draw_series(line_series)?;
+
+Ok(())
 }
