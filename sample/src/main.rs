af1d228d28d358c3ccc0d502e523dfd7b623c457
ytakasugi <sh7.tibi0129@gmail.com>
2022-01-23 23:04:30 +0900

    change file

--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,7 +1,23 @@
-use chrono::*;
+fn total(init: i32, n: i32) -> i32 {
+    (0..=n).fold(init, |sum, item| sum + item)
+}
+
 fn main() {
-    let time_str = "2021-01-01 23:30";
-    let date = Local.datetime_from_str(time_str, "%Y-%m-%d %H:%M")
-                            .unwrap();
-    println!("{}", date);
+    let v = vec![1, 2, 3, 4];
+    let mut iter = v.iter();
+
+    assert_eq!(iter.next(), Some(&1));
+    assert_eq!(iter.next(), Some(&2));
+    assert_eq!(iter.next(), Some(&3));
+    assert_eq!(iter.next(), Some(&4));
+    assert_eq!(iter.next(), None);
+
+    let x = &mut [1, 2, 4];
+    for elem in x.iter_mut() {
+        *elem += 2;
+    }
+
+    assert_eq!(x, &[3, 4, 6]);
+
+    println!("{}", total(0, 10));
 }
