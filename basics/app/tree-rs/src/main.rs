aeaa557f89af8b499cce66a287552e7761e6478f
ytakasugi <sh7.tibi0129@gmail.com>
2022-02-13 20:51:23 +0900

    change file

--- basics/app/tree-rs/src/main.rs
+++ basics/app/tree-rs/src/main.rs
@@ -1,3 +1,34 @@
+use std::{env, path};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let mut target_dir = ".";
+
+    if args.len() >= 2 {
+        target_dir = &args[1];
+    }
+
+    let target = path::PathBuf::from(target_dir);
+    println!("{}", target_dir);
+    tree(&target, 0);
+}
+
+fn tree(target: &path::Path, level: isize) {
+    let files = target.read_dir().expect("Non-existent path.");
+    
+    for entry in files {
+        let path = entry.unwrap().path();
+
+        for _ in 1..=level {
+            print!("|  ");
+        }
+        let file_name = path.file_name().unwrap().to_string_lossy();
+
+        if path.is_dir() {
+            println!("|-- <{}>", file_name);
+            tree(&path, level + 1);
+            continue;
+        }
+        println!("|-- {}", file_name);
+    }
 }
