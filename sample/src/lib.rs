b190568d17e48c449ddd657caff8b19d5599bfe6
ytakasugi <sh7.tibi0129@gmail.com>
2023-01-01 00:39:43 +0900

    change files

--- sample/src/lib.rs
+++ sample/src/lib.rs
@@ -1,39 +1,22 @@
-use std::thread;
+use std::fs::File;
 
-pub fn init_logger(){
-    let base_config = fern::Dispatch::new();
 
-    let file_config = fern::Dispatch::new()
-        .level(log::LevelFilter::Error)
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{}[{}][{}][{:?}] {}",
-                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S.%H%M%S]"),
-                record.level(),
-                record.target(),
-                thread::current().id(),
-                message
-            ))
-        })
-        .chain(fern::log_file("error.log").unwrap());
+pub fn init_logger() -> std::io::Result<()> {
+    let _log = File::create("application.log")?;
 
-    let stdout_config = fern::Dispatch::new()
-        .level(log::LevelFilter::Debug)
-        .format(|out, message, record| {
-            out.finish(format_args!(
-                "{}[{}][{}][{:?}] {}",
-                chrono::Local::now().format("[%Y-%m-%d %H:%M:%S.%H%M%S]"),
-                record.level(),
-                record.target(),
-                thread::current().id(),
-                message
-            ))
-        })
-        .chain(fern::log_file("app.log").unwrap());
+    // ログ出力を設定する
+    let format = tracing_subscriber::fmt::format()
+        .with_level(true)
+        .with_target(false)
+        .with_thread_ids(true)
+        .with_thread_names(true) 
+        .compact();
 
-    base_config
-        .chain(file_config)
-        .chain(stdout_config)
-        .apply()
-        .unwrap();
+
+    
+    tracing_subscriber::fmt()
+        .event_format(format)
+        .init();
+    
+    Ok(())
 }
--- sample/src/main.rs
+++ sample/src/main.rs
@@ -1,19 +1,22 @@
-use std::thread;
-
 use sample::init_logger;
 
-const MAX_THREAD: i32 = 10;
-
 #[tokio::main]
-async fn main() {
-    init_logger();
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    init_logger()?;
     // 論理コア数を表示
     log::info!("Logical cores: {}", num_cpus::get());
 
-    for _ in 0..MAX_THREAD {
-        tokio::spawn(async {
-            log::info!("[{:?}]", thread::current().id());
-        });
+    let threads: Vec<_> = (0..10)
+        .map(|i| {
+            tokio::spawn(async move {
+                tracing::info!("{}", i);
+            })
+        })
+        .collect();
+
+    for thread in threads {
+        thread.await?;
     }
-    log::info!("[{:?}]", thread::current().id());
+
+    Ok(())
 }
